1d1e9e9c82fdd6cf6e394472433c4f129f738d1d
Conor Donnelly <donn0021@umn.edu>
2022-11-22 21:04:16 -0600

    set up database and seeded with SQL

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Management"),
+       ("Clowns"),
+       ("Treats"),
+       ("Snacks");
+       
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Associate", 42000, 1),
+       ("Bigtime Manager", 145000, 2),
+       ("Performing Clown", 29900, 3),
+       ("Candy Maker", 37500, 4),
+       ("Nachos Designer", 41000, 5);
+
+       INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Chuck", "Knoblauch", 1, 6),
+       ("Ricardo", "Mantleban", 2, 7),
+       ("Benjamin", "Franklin", 3, 8),
+       ("Wayne", "Gretzky", 4, 9),
+       ("Werner", "Herzon", 5, 10);
+       ("DJ Jazzy", "Jeff", 6, 1),
+       ("Deltron", "3030", 7, 2),
+       ("Bionic", "Beaver", 8, 3),
+       ("Focal", "Fossa", 9, 4),
+       ("Intrepid", "Ibex", 10, 5);
